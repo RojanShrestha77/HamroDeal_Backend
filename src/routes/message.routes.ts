36bc37ccbffac601030130e1f0972e26c02669f7
RojanShrestha77 <shresrozan555@gmail.com>
2026-02-24 16:07:54 +0545

    inetgrated message reoutes

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -5,19 +5,14 @@ import { authorizedMiddleware } from "../middlewares/authorized.middleware";
 const router = Router();
 const messageController = new MessageController();
 
-// All message routes require authentication
 router.use(authorizedMiddleware);
 
-// Send a message
 router.post("/", messageController.sendMessage);
 
-// Get messages for a conversation
 router.get("/conversation/:conversationId", messageController.getMessages);
 
-// Delete a message
 router.delete("/:id", messageController.deleteMessages);
 
-// Mark messages as read in a conversation
 router.patch("/conversation/:id/read", messageController.markAsRead);
 
 export default router;
